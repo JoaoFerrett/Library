5daf734894461f9553d010d530c01cb04d06158b
JoaoFerrett <123ferrett@gmail.com>
2026-09-01 00:42:39 -0300

    Adding getters and setters

--- src/library/Author.java
+++ src/library/Author.java
@@ -2,10 +2,42 @@ package library;
 
 public class Author {
 	
-	String name;
-	String birthDate;
-	String deathDate;
-	String placeOfBirth;
+	private String name;
+	private String birthDate;
+	private String deathDate;
+	private String placeOfBirth;
+	
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getBirthDate(){
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate){
+		this.birthDate = birthDate;
+	}
+
+	public String getDeathDate(){
+		return deathDate;
+	}
+
+	public void setDeathDate(String deathDate){
+		this.deathDate = deathDate;
+	}
+
+	public String getPlaceOfBirth(){
+		return placeOfBirth;
+	}
+
+	public void setPlaceOfBirth(String placeOfBirth){
+		this.placeOfBirth = placeOfBirth;
+	}
 	
 	void showDetails() {
 		
--- src/library/Book.java
+++ src/library/Book.java
@@ -2,16 +2,57 @@ package library;
 
 public class Book {
 	
-	double price;
-	String title;
-	String description;
-	String id;
-	Author author;
+	private double price;
+	private String title;
+	private String description;
+	private String id;
+	private Author author;
 	
 	public Book() {
 		System.out.println("A new book has benn created! ");
 	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 	
+	public String getTitle(){
+		return title;
+	}
+
+	public void setTitle(String title){
+		this.title = title;
+	}
+
+	public String getDescription(){
+		return description;
+	}
+
+	public void setDescription(String description){
+		this.description = description;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public void setId(String id){
+		this.id = id;
+	}
+
+	public Author geAuthor(){
+		return author;
+	}
+
+	public void setAuthor(Author author){
+		this.author = author;
+	}
+
+
 	void showDetails() {
 		
 		System.out.println("Showing book details: ");
@@ -26,6 +67,7 @@ public class Book {
 		System.out.println("---------------");
 
 	}
+	
 	
 	public boolean aplliesDiscount(double porcentage) {
 		
